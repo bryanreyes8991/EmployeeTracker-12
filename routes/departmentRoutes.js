1f3ff8a397c71a2109c43599fccb2d8018204f56
Nebula Rider <bryanreyes8991@gmail.com>
2023-11-14 18:14:05 -0600

    built employee routes

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const db = require('../config/connection').mysql();
 
-getAllDpts () = router.get('/', (req, res) => {
+viewAllDpts () = router.get('/', (req, res) => {
     const sql = `SELECT id, department_name FROM department`;
     db.query(sql, (err, departments) => {
         if (err) {
@@ -12,17 +12,17 @@ getAllDpts () = router.get('/', (req, res) => {
 });
 
 addDpt () = router.post('/', ({ body }, res) => {
-    if(!body || !body.deparment_name) {
+    if(!body || !body.department_name) {
         return res.status(500).json({ message: 'error', error: err.message});
     }
     const sql = `INSERT INTO department (department_name) VALUES (?)`;
-    const params = [body.deparment_name];
-    db.query(sql, params, (err, params) => {
+    const department = [body.deparment_name];
+    db.query(sql, params, (err, department) => {
         if (err) {
             return res.status(500).json({ message: 'error', error: err.message });
         }
-        console.table(params);
+        console.table(department);
     });
 });
 
-module.exports = addDpt() , getAllDpts();
+module.exports = addDpt() , viewAllDpts();
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,5 +1,33 @@
-// VIEW EMPLOYEES
-// GET EMPLOYEES
-// ADD EMPLOYEE
-// UPDATE EMPLOYEE-ROLE ?
-// 
+const router = require('express').Router();
+const db = require('../config/connection').mysql();
+
+viewAllEmps () = router.get('/', (req, res) => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary AS salary, department.name AS department,
+    manager.first_name AS manager FROM employee 
+    LEFT JOIN employee manager ON  manager.id = employee.manager_id
+    JOIN role ON employee.role_id = role.id 
+    JOIN department ON role.department_id = department.id 
+    ORDER BY employee.id;`;
+      db.query(sql, (err, employees) => {
+        if (err) {
+            return res.status(500).json({ message: 'error', error: message });
+        }
+        console.table(employees);
+      });
+});
+
+addEmp () = router.post('/', ({ body }, res) => {
+    if(!body || body.employee.id) {
+        return res.status(500).json({ message: 'error', error: err.message });
+    }
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ?, ?)`;
+    db.query(sql, { body }, (err, employee) => {
+        if (err) {
+            return res.status(500).json({ message: 'error', error: err.message});
+        }
+        console.table(employee)
+    });
+});
+
+module.exports = viewAllEmps() , addEmp();
